3f676191c1cea0fe0ddeb4a4e55d7557a7ca2408
jyoti4upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2024-11-12 00:36:17 +0000

    conversion of datatype

---
+++ basics/conOp.js
@@ -0,0 +1,55 @@
+/*let score=33
+let score1="33"
+let sc="33abc"
+//const {score2}=req.body // datatype is not known
+
+console.log(typeof score);
+console.log(typeof(score1));
+
+let valueInNumber=Number(score1) //string convert to number
+console.log(typeof valueInNumber);
+
+let v=Number(sc); //convert toh hogya lekin type nan hai
+console.log(typeof v); 
+console.log(v); //give nan because type is confirm
+
+let sco=null;
+let c=Number(sco);
+console.log(typeof c);
+console.log(c);
+
+let st="jyoti"
+let t=Number(st)
+console.log(t);
+
+let ep=true;
+let p=Number(ep)
+console.log(p);
+
+let ms=undefined
+let s=Number(ms)
+console.log(s); */
+
+// "33"=>33
+//"33bc"=> NaN (type=number)
+// true=>1 false=>0
+//null=>0
+
+/*let isLoggedIn=1
+
+let booLoggedIn=Boolean(isLoggedIn)
+console.log(booLoggedIn);*/
+
+// 1=>true 0=>false
+//"jyoti"=>true
+//""=>false 
+
+let someNo=33
+let stringNo=String(someNo)
+console.log(stringNo);
+console.log( typeof (stringNo))
+
+
+
+
+
