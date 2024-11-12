2fb5247a1f9eb8508f24d9e7eff01d83ca9ee9fb
jyoti4upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2024-11-12 01:02:38 +0000

    operation

---
+++ basics/operation.js
@@ -0,0 +1,26 @@
+/*let value=3
+let negValue=-value
+//console.log(negValue);
+
+let str1="hello"
+let str2="jyoti"
+let str3=str1+str2
+console.log(str3);
+
+console.log("2"+1);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log("1"+(2+2));
+console.log(3+4*5%3);*/
+
+//console.log(+true);
+//console.log(+"");
+
+let num1,num2,num3
+
+let gameCounter=100
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
