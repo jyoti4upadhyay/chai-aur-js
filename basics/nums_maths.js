f6dd737a749d3a167d77e1b481b0aa71214f0f2b
jyoti4upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2024-11-14 02:21:42 +0000

    nums_maths

---
+++ basics/nums_maths.js
@@ -0,0 +1,42 @@
+/*const score=400
+console.log(score);//inspect pe jaakr prototype bhi check kr skte hai iske
+
+
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));//decimal k baad kitni value chahiye
+
+//const otherNumber=23.8966;
+//const otherNumber=123.8966;
+const otherNumber=1123.8966;
+console.log(otherNumber.toPrecision(3));
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString());//default value in us
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+console.log(Number.MAX_VALUE)//more properties are there*/
+
+//++++++++++++++++Maths++++++++++++++++++++
+
+//console.log(Math);//Inspect=>console
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.6));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.9));
+//console.log(Math.min(4,3,6,8));
+console.log(Math.random());//decimal aati hai vale b/w 0-9
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
