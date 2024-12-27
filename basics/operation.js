614f2b0045fe88d3b3e04894e04113ab992e284e
jyoti4upadhyay <120690881+jyoti4upadhyay@users.noreply.github.com>
2024-12-27 17:15:42 +0000

    op

--- basics/operation.js
+++ basics/operation.js
@@ -3,24 +3,26 @@ let negValue=-value
 //console.log(negValue);
 
 let str1="hello"
-let str2="jyoti"
+let str2=" jyoti"
 let str3=str1+str2
-console.log(str3);
+console.log(str3);*/
 
 console.log("2"+1);
 console.log(1+"2");
 console.log("1"+2+2);
 console.log(1+2+"2");
 console.log("1"+(2+2));
-console.log(3+4*5%3);*/
+console.log(3+4*5%3);
 
-//console.log(+true);
-//console.log(+"");
+//console.log(+true);//=>1
+//console.log(true+)//=>error
+//console.log(+"");//=>0
 
-let num1,num2,num3
+/*let num1,num2,num3
+num1=num2=num3=2+2 //not a better way
 
 let gameCounter=100
-//gameCounter++;
+//gameCounter++; // prefix and postfix in js mdn
 ++gameCounter;
-console.log(gameCounter);
+console.log(gameCounter);*/
 
